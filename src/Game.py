492be9f47fa0b2f3ddb64d74c23a91a3c7a261b8
EdgarsFeic69 <edgarsfeic1569@gmail.com>
2021-10-12 09:43:38 -0500

    master

--- src/Game.py
+++ src/Game.py
@@ -1,9 +1,10 @@
-# from Fruit import Fruit
-# from Gameboard import Gameboard
-# from Snake import Snake
 from random import randint
 from fpstimer import FPSTimer
 import sys, os
+
+_GREEN = '\033[32;1m'
+_RED = '\033[31;1m'
+_WHITE = '\033[0m'
 
 def clear():
     if os.name == 'nt':
@@ -35,7 +36,11 @@ class Game:
     def startGame(self):
         '''Función que inicia el juego'''
         self.spawnSnake()
-        self.spawnFruit()
+        # self.spawnFruit()
+
+        self._fruitx = (self._width//2) + 1
+        self._fruity = (self._height//2) + 8
+
         self.update()
         self.show()
 
@@ -57,7 +62,8 @@ class Game:
             self._gameboard[-1][i] = '='
 
         # Titulo de arriba XD
-        title = ' Python en Python xd '
+        snake = '\U0001F40D'
+        title = f' Python en Python xd '
         mid_title = self._width//2
         mid_text = len(title)//2
         for i in range(len(title)):
@@ -68,10 +74,10 @@ class Game:
         self._fruitx = randint(0, self._width-1)
         self._fruity = randint(0, self._height-1)
 
-        self._fruitx = self._width//2
-        self._fruity = self._height//2
+        # self._fruitx = self._width//2
+        # self._fruity = self._height//2
 
-        self._gameboard[self._fruity][self._fruitx] = '@'
+        # self._gameboard[self._fruity][self._fruitx] = '@'
 
         while True:
             if self._gameboard[self._fruity][self._fruitx] != ' ':
@@ -108,12 +114,17 @@ class Game:
 
     def update(self):
         '''Actualiza el juego'''
-        # self._gameboard[self._fruity][self._fruitx] = '@'
+        # Coloca la fruta en el tablero
+        self._gameboard[self._fruity][self._fruitx] = f'{_RED}@{_WHITE}'
 
+
+        
+        # Vacia los caracteres de la serpiente
         self._gameboard[self._snakeY][self._snakeX] = ' '
         for i in range(self._snake_len):
             self._gameboard[self._snake[i][0]][self._snake[i][1]] = ' '
 
+        # Aumenta la direccion de la serpiente
         self._snakeX += self._snakeDir[0][1]
         self._snakeY += self._snakeDir[0][0]
         self._snake[0][0] = self._snakeY
@@ -121,31 +132,54 @@ class Game:
         for i in range(self._snake_len-1):
             self._snake[i+1][0] += self._snakeDir[i+1][0]
             self._snake[i+1][1] += self._snakeDir[i+1][1]
-            # print(self._snakeDir[i])
-            # print(self._snakeDir[i-1])
-            self._snakeDir[i+1] = self._snakeDir[i]
 
+        # Comprueba que no se salga del rango
+        # Comprueba si se comio la fruta
+        if self._snakeX == self._fruitx and self._snakeY == self._fruity:
+            self._gameboard[self._snakeY][self._snakeX] = ' '
+            self.eatFruit()
+        elif self._gameboard[self._snakeY][self._snakeX] != ' ':
+            self._death = True
+            return
+
+        # Establece la direccion
+        for i in range(self._snake_len-1):
+            if self._snakeDir[i+1] != self._snakeDir[i]:
+               self._snakeDir[i+1] = self._snakeDir[i]
+               break
+
+        # Dibuja la serpiente
         for i in range(self._snake_len):
-            self._gameboard[self._snake[i][0]][self._snake[i][1]] = '0'
-        self._gameboard[self._snakeY][self._snakeX] = 'Q'
+            self._gameboard[self._snake[i-1][0]][self._snake[i-1][1]] = f'{_GREEN}0{_WHITE}'
+        self._gameboard[self._snakeY][self._snakeX] = f'{_GREEN}O{_WHITE}'
+
+
+    def eatFruit(self):
+        if self._snakeX == self._fruitx and self._snakeY == self._fruity:
+            self._score += 100
+            self._snake_len += 1
+
+            tempDir = [self._snakeDir[-1][0], self._snakeDir[-1][1]]
+            tempCoord = [self._snake[-1][0], self._snake[-1][1]]
+
+            tempCoord[0] -= tempDir[0]
+            tempCoord[1] -= tempDir[1]
+
+            self._snakeDir.append(tempDir)
+            self._snake.append(tempCoord)
+            # self._snake[-1][-1] -= self._snakeDir[-1][-1]
+            self.spawnFruit()
 
 
     def isDeath(self):
-        # for i in range(self._height):
-        #     self._gameboard[i][0] = '|'
-        #     self._gameboard[i][-1] = '|'
-        # # Borde superior
-        # for i in range(self._width):
-        #     self._gameboard[0][i] = '='
-        #     self._gameboard[-1][i] = '='
         if self._snakeX == 0 or self._snakeX == self._width-1:
             self._death = True
         if self._snakeY == 0 or self._snakeY == self._height-1:
             self._death == True
         
     def loop(self):
-        timer = FPSTimer(15)
-        self._snakeDir[0] = [1,0]
+        timer = FPSTimer(10)
+        # self._snakeDir[0] = [1,0]
         while(True):
             clear()
             self.update()
